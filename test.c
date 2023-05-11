4da01ea58643d30b4f1ceacbf28d56ad2e75c9c3
Jake Esperson <jake.esperson@outlook.com>
2023-05-10 23:37:35 -0700

    Changed test.c to include segfault

--- test.c
+++ test.c
@@ -4,11 +4,12 @@
 int main(int argc, char **argv)
 {
     int *x = malloc(10 * sizeof(int));
+    char *str = "Your number:";
     int a;
     printf("Please type a number!\n");
     scanf("%d", &a);
-    x[15] = a;                                                                       // FIXME: write out of bounds
-    printf("You typed %d! The number at the end of the array is %d\n", x[15], x[9]); // FIXME: read out of bounds and uninitialized value
-    // FIXME: free(p)
+    *(str + 1) = a;                                                      // FIXME: Cannot modify a string literal
+    printf("%s! The number at the end of the array is %d\n", str, x[9]); // FIXME: read from uninitialized memory
+    // FIXME: free(x);
     return 0;
 }
